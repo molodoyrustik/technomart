f442ef64187e0611aeacc3894ab7a22e5ebe4515
Bagautdinov Ruslan <molodoybaja@gmail.com>
2019-04-06 02:09:53 +0500

    начал писать css для header

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,28 @@
 (function() {
   'use strict';
+  const searchInput = document.querySelector('.header__search');
+  const searchPlaceholder = document.querySelector('.header__search-placeholder');
+  const searchIcon = document.querySelector('.header__search-icon');
 
-  var link = document.querySelector(".user-block__login");
-  var popup = document.querySelector(".modal");
-  if (popup) {
-    var close = popup.querySelector(".modal__close");
-    var form = popup.querySelector("form");
-    var login = popup.querySelector("[name=login]");
-    var password = popup.querySelector("[name=password]");
-    var storage = localStorage.getItem("login");
+  searchInput.addEventListener('mouseover', () => {
+    searchIcon.classList.add('header__search-icon--hover')
+    searchInput.classList.add('header__search--hover');
+  })
+  searchInput.addEventListener('mouseout', () => {
+    searchIcon.classList.remove('header__search-icon--hover')
+    searchInput.classList.remove('header__search--hover');
+  })
 
-    var map = document.querySelector('.map');
-    var mapClose = map.querySelector('.map__close');
-    var mapLink = document.querySelector('.contact-info__link');
-
-    mapLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      map.classList.add('map__show');
-    })
-
-    mapClose.addEventListener('click', (event) => {
-      event.preventDefault();
-      map.classList.remove('map__show');
-    })
-
-
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.add("modal__show");
-      if (storage) {
-        login.value = storage;
-        password.focus();
-      } else {
-        login.focus();
-      }
-    });
-    
-    close.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.remove("modal__show");
-      popup.classList.remove("modal__error");
-    });
-
-    form.addEventListener("submit", function(event) {
-      if (!login.value || !password.value) {
-        event.preventDefault();
-        popup.classList.add("modal__error");
-      } else {
-        localStorage.setItem("login", login.value);
-      }
-    });
-
-    window.addEventListener("keydown", function(event) {
-      if (event.keyCode === 27) {
-        if (popup.classList.contains("modal__show")) {
-          popup.classList.remove("modal__show");
-          popup.classList.remove("modal__error");
-        }
-      }
-    });
-  }
-
+  searchInput.addEventListener('focus', (event) => {
+    searchIcon.classList.add('header__search-icon--focus')
+    searchPlaceholder.classList.add('header__search-placeholder--focus');
+    searchInput.classList.add('header__search--focus');
+  })
+  searchInput.addEventListener('blur', (event) => {
+    searchIcon.classList.remove('header__search-icon--focus')
+    searchPlaceholder.classList.remove('header__search-placeholder--focus');
+    searchInput.classList.remove('header__search--focus');
+    searchInput.value = '';
+  })
+  
 })();
